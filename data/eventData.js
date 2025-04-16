badf56e79bacdcf7c7a2bad99dbd461fce653b53
박승희 <ridia05031@gmail.com>
2025-04-16 11:12:49 +0900

    [#2] feat: 종료된 이벤트 이미지 및 전체 이벤트 데이터 추가

---
+++ data/eventData.js
@@ -0,0 +1,40 @@
+const eventData = [
+  {
+    id: 1,
+    title: '오디 이벤트! 드라마&영화 속 명소 이야기 재생 이벤트',
+    period: '2025. 4. 10. ~ 2025. 4. 23.',
+    image: 'event1.png',
+  },
+  {
+    id: 2,
+    title: 'TourAPI 활용 이벤트 ‘맞춤형데이터 in TourAPI’',
+    period: '2025. 4. 10. ~ 2025. 4. 23.',
+    image: 'event2.png',
+  },
+  {
+    id: 3,
+    title: '봄꽃 여행에 관한 궁금증을 1330 문자 채팅으로 물어보세요!',
+    period: '2025. 4. 1. ~ 2025. 4. 29.',
+    image: 'event3.png',
+  },
+  {
+    id: 4,
+    title: '월간 밤밤곡곡 3월! 벚꽃 사진 이벤트',
+    period: '2025. 3. 24. ~ 2025. 4. 20.',
+    image: 'event4.png',
+  },
+  {
+    id: 5,
+    title: '자전거 자유여행 대표코스 60선 이벤트',
+    period: '2025. 3. 19. ~ 2025. 5. 31.',
+    image: 'event5.png',
+  },
+  {
+    id: 6,
+    title: '오디EVENT! 봄맞이 여행 퀴즈 이벤트',
+    period: '2025. 3. 13. ~ 2025. 3. 27.',
+    image: 'event6.png',
+  },
+];
+
+export default eventData;
