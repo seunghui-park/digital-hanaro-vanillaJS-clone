75ec2aef2dc23d1237e521dacc4cfa874fa21bc9
ggamD00 <jaehuek@naver.com>
2025-04-16 14:34:24 +0900

    [#10] feat: 홈, 이벤트, 지역 페이지에 footer 적용
    footer 하단 고정을 위해 body에 "flex flex-col min-h-screen", 메인컨텐츠에 "flex-grow" 적용

--- scripts/common.js
+++ scripts/common.js
@@ -15,3 +15,21 @@ document.addEventListener('DOMContentLoaded', function () {
       document.getElementById('header').appendChild(wrapper);
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  let footerPath = 'components/footer.html';
+  if (location.pathname.includes('/pages/')) {
+    footerPath = '../components/footer.html';
+  } else {
+    footerPath = 'components/footer.html';
+  }
+
+  fetch(footerPath)
+    .then((res) => res.text())
+    .then((data) => {
+      const wrapper = document.createElement('div');
+      wrapper.classList.add('relative');
+      wrapper.innerHTML = data;
+      document.getElementById('footer').appendChild(wrapper);
+    });
+});
